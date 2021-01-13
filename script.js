b02d7b53beb0fe1e5d68eeffec915bc4a57efabf
Aleksandr Momziakov <73888668+Sasanano@users.noreply.github.com>
2021-01-13 12:05:25 +0300

    Change1

---
+++ script.js
@@ -0,0 +1,26 @@
+const $start = document.querySelector('#start')
+const $game = document.querySelector('#game')
+
+$start.addEventListener('click', startGame)
+$game.addEventListener('click', handleBoxClick)
+
+
+function startGame() {
+    $game.style.backgroundColor = '#fff'
+    $start.classList.add('hide')
+
+    renderBox()
+}
+
+function renderBox() {
+    const box = document.createElement('div')
+
+    box.style.height = box.style.width = '50px'
+    box.style.position = 'absolute'
+    box.style.backgroundColor = '#000'
+    box.style.top = '50px'
+    box.style.left = '70px'
+    box.style.cursor = 'pointer'
+
+    $game.insertAdjacentElement('afterbegin', box)
+}
